060463e61dd1a48e6a4ae37609ada2423f0f22af
mdiogc <145054405+mdiogc@users.noreply.github.com>
2024-03-18 16:35:29 +0100

    Add files via upload

---
+++ ut4/ejer/funciones/deco_sort.py
@@ -0,0 +1,14 @@
+"""
+¿Sabría implementar un decorador para ordenar el resultado de cualquier función
+tomando un parámetro opcional que indique si la ordenación es ascendente o descendente?
+
+    Entrada = 8,1,4,2,5
+    Salida = 2, 4, 8
+"""
+def sort (asc = True):
+
+
+
+@sort()
+def extract_evens (*values): [3, 1, 4, 2]
+return (v for v in values if v % 2 ==0 )
