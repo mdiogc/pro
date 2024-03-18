15a339e5cb5cc3770ba9b39f615b769558b51661
mdiogc <145054405+mdiogc@users.noreply.github.com>
2024-03-18 17:09:50 +0100

    Add files via upload

---
+++ ut4/ejer/funciones/factorial_recursive.py
@@ -0,0 +1,11 @@
+# *******************************************************
+# CALCULANDO EL FACTORIAL DE UN NÚMERO (CON RECURSIVIDAD)
+# *******************************************************
+
+
+def factorial(n: int) -> int:
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return n
