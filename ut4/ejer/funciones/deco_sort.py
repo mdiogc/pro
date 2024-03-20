e692aa257e16432184bae8892241d399836e7a38
mdiogc <145054405+mdiogc@users.noreply.github.com>
2024-03-20 02:54:43 +0100

    Update deco_sort.py

--- ut4/ejer/funciones/deco_sort.py
+++ ut4/ejer/funciones/deco_sort.py
@@ -6,9 +6,26 @@ tomando un parámetro opcional que indique si la ordenación es ascendente o des
     Salida = 2, 4, 8
 """
 def sort (asc = True):
+    def decorator (func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if isinstance(result, (list, tuple)):
+                if asc:
+                    sorted_result = sorted(result)
+                else:
+                    sorted_result = sorted(result, reverse=True)
+            return sorted_result
+        return wrapper
+    return decorator
+
+
 
 
 
 @sort()
-def extract_evens (*values): [3, 1, 4, 2]
-return (v for v in values if v % 2 ==0 )
+def extract_evens (*values):
+    return (v for v in values if v % 2 ==0)
+
+
+sorted_result = extract_evens(8,1,4,2,5)
+print(sorted_result)
