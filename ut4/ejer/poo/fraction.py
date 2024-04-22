14ad02afd1d1c77111b0044d533c780f96af8270
mdiogc <145054405+mdiogc@users.noreply.github.com>
2024-04-22 16:26:35 +0100

    Update fraction.py

--- ut4/ejer/poo/fraction.py
+++ ut4/ejer/poo/fraction.py
@@ -1,18 +1,31 @@
 from __future__ import annotations
 
 
+
 class Fraction:
     def __init__(self, num: int, den: int):
         self.num = num
         self.den = den
 
-    def __add__(self, other: Fraction): ...
+    def __add__(self, other: Fraction):
+        new_num = self.num * other.den + other.num * self.den
+        new_den = self.den * other.den
+        return Fraction(new_num, new_den)
 
-    def __sub__(self, other: Fraction): ...
+    def __sub__(self, other: Fraction):
+        new_num = self.num * other.den - other.num * self.den
+        new_den = self.den * other.den
+        return Fraction(new_num, new_den)
 
-    def __mul__(self, other: Fraction): ...
+    def __mul__(self, other: Fraction):
+        new_num = self.num * other.den
+        new_den = other.num * self.den
+        return Fraction(new_num, new_den)
 
-    def __truediv__(self, other: Fraction): ...
+    def __truediv__(self, other: Fraction):
+        new_num = self.num / other.den
+        new_den = other.num / self.den
+        return Fraction(new_num, new_den)
 
     def __str__(self): ...
 
